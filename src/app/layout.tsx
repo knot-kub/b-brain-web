7d550762c5c332e8861ddbb551c3a2318d64f9d7
Swiss Lertpattanasak <swiss.knot@gmai.com>
2025-09-10 22:17:44 +0700

    feat: init http client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 
 import { SidebarProvider } from '@/context/SidebarContext';
 import { ThemeProvider } from '@/context/ThemeContext';
+import { HttpClient } from '@/http/http.client';
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -13,6 +14,28 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // instantiate HttpClient singleton
+  HttpClient.instantiate({
+    private: {
+      host: process.env.API_BASE_URL || 'http://localhost:8080',
+      timeout: 10000,
+      interceptorOptions: {
+        request: {
+          tokenHandler: () => localStorage.getItem('token') || '',
+        },
+        response: {},
+      },
+    },
+    public: {
+      host: process.env.API_BASE_URL || 'http://localhost:8080',
+      timeout: 10000,
+      interceptorOptions: {
+        request: {},
+        response: {},
+      },
+    },
+  })
+
   return (
     <html lang="en">
       <body className={`${outfit.className} dark:bg-gray-900`}>
--- src/http/http.interface.ts
+++ src/http/http.interface.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpStatusCode, AxiosResponse } from 'axios'
 import { AuthService, UserService } from '../services'
 
